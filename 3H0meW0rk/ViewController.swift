6c73727a700920ac23c28052ac735c44cf982c09
baygeldi <akylbekuulubaygeldi@gmail.com>
2022-12-12 18:21:35 +0600

    ccc

--- 3H0meW0rk/ViewController.swift
+++ 3H0meW0rk/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var mails: [String] = ["Байгелди - 5мкр 44дом ","Нурик - 3мкр 23дом","Зарин - 4мкр 1дом","Нурай - 7мкр 21дом","Эмилия - 7мкр 5дом","Магдиэл - 5мкр 7дом","Азия - казахстан Алма-Ата"]
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,4 +24,17 @@ class ViewController: UIViewController {
 
 
 }
-
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mails.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "adress", for: indexPath)
+        
+        cell.textLabel?.text = mails[indexPath.row]
+        
+        return cell
+    }
+    
+}
